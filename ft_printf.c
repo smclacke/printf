c385bfcef9a5dadc19d73dbfc1373fdcf7a0e6b8
Sarah <115113929+smclacke@users.noreply.github.com>
2022-11-19 13:52:27 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: SarahLouise <SarahLouise@student.42.fr>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/05 11:04:40 by smclacke          #+#    #+#             */
-/*   Updated: 2022/11/17 16:35:02 by SarahLouise      ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_printf.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: SarahLouise <SarahLouise@student.42.fr>      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/05 11:04:40 by smclacke      #+#    #+#                 */
+/*   Updated: 2022/11/19 13:52:21 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,41 +82,3 @@ int	ft_printf(const char *str, ...)
 	va_end(valist);
 	return (char_count);
 }
-// x X and i test
-
-// int main(void)
-// {
-//     unsigned long         x = 85769887;
-//     long long             i = 85769887;
-//     unsigned long long    X = 85769887;
-    
-//     ft_printf("GIVE ME THE UPPER HEXA: %X\nGIVE ME THE LOWER HEXA: %x\nGIVE ME THE DECA: %i\n", X, x, i);
-// }
-
-// p test
-
-// int main()
-// {
-// 	int x = 1997;
-// 	int j = 1882;
-// 	int *ptr = &x;
-	
-// 	x = j;
-// 	j = x;
-	
-// 	printf("The address is: %p, the value is %d\n", ptr, *ptr);
-// 	ft_printf("The address is: %p, the value is %d\n", ptr, *ptr);
-// }
-
-// c s d u % test
-
-// int main()
-// {
-// 	int           c = 'G';
-// 	int           d = 89;
-// 	unsigned int  u = 768;
-// 	char          *str = "a string";
-// 	char          perc = '%';
-
-// 	ft_printf("c: %c\nd: %d\nu: %u\nstr: %s\nperc: %%\n", c, d, u, str, perc);
-// }
---
+++ printf_main.c
@@ -0,0 +1,40 @@
+
+
+// x X and i test
+
+// int main(void)
+// {
+//     unsigned long         x = 85769887;
+//     long long             i = 85769887;
+//     unsigned long long    X = 85769887;
+
+//     ft_printf("GIVE ME THE UPPER HEXA: %X\nGIVE ME THE LOWER HEXA: %x\nGIVE ME THE DECA: %i\n", X, x, i);
+// }
+
+// p test
+
+// int main()
+// {
+// 	int x = 1997;
+// 	int j = 1882;
+// 	int *ptr = &x;
+	
+// 	x = j;
+// 	j = x;
+	
+// 	printf("The address is: %p, the value is %d\n", ptr, *ptr);
+// 	ft_printf("The address is: %p, the value is %d\n", ptr, *ptr);
+// }
+
+// c s d u % test
+
+// int main()
+// {
+// 	int           c = 'G';
+// 	int           d = 89;
+// 	unsigned int  u = 768;
+// 	char          *str = "a string";
+// 	char          perc = '%';
+
+// 	ft_printf("c: %c\nd: %d\nu: %u\nstr: %s\nperc: %%\n", c, d, u, str, perc);
+// }
