9ac296f8d42fafebfc797a0c4b38f528ae921c66
Sarah <115113929+smclacke@users.noreply.github.com>
2022-11-19 22:55:11 +0100

    Add files via upload

--- printf_main.c
+++ printf_main.c
@@ -11,21 +11,6 @@
 //     ft_printf("GIVE ME THE UPPER HEXA: %X\nGIVE ME THE LOWER HEXA: %x\nGIVE ME THE DECA: %i\n", X, x, i);
 // }
 
-// p test
-
-// int main()
-// {
-// 	int x = 1997;
-// 	int j = 1882;
-// 	int *ptr = &x;
-	
-// 	x = j;
-// 	j = x;
-	
-// 	printf("The address is: %p, the value is %d\n", ptr, *ptr);
-// 	ft_printf("The address is: %p, the value is %d\n", ptr, *ptr);
-// }
-
 // c s d u % test
 
 // int main()
@@ -40,40 +25,72 @@
 // }
 
 
-int main()
-{
-	int      	     c = 'G';
-	int        	 	  d = 967;
-	int				x = 955;
-	int 			*ptr = &x;
-	unsigned int		X = 386969449;
-	
-	unsigned int  u = 768;
-	char          *str = "a string";
-	char          perc = '%';
+//more testing
 
-	ft_printf("c: %c\n", c);
-	printf("c: %c\n", c);
-	ft_printf("s: %s\n", str);
-	printf("s: %s\n", str);
-	ft_printf("The address is: %p, the value is %d\n", ptr, *ptr);
-	printf("The address is: %p, the value is %d\n", ptr, *ptr);
-	ft_printf("d: %d\n", d);
-	printf("d: %d\n", d);
-	ft_printf("x: %x\n", x);
-	printf("x: %x\n", x);
-	ft_printf("X: %X\n", X);
-	printf("X: %X\n", X);
-	ft_printf("u: %u\n", u);
-	printf("u: %u\n", u);
-	ft_printf("%%");
-	printf("\n");
-	printf("%%\n");
-	ft_printf("%%%");
-	printf("\n");
-	printf("%%%");
-	printf("\n");
-}
+// int main()
+// {
+// 	int      	     c = 'G';
+// 	int        	 	  d = 967;
+// 	int				x = 955;
+// 	int 			*ptr = &x;
+// 	unsigned int		X = 386969449;
+	
+// 	unsigned int  u = 768;
+// 	char          *str = "a string";
+// 	char          perc = '%';
+
+// 	ft_printf("c: %c\n", c);
+// 	printf("c: %c\n", c);
+// 	ft_printf("s: %s\n", str);
+// 	printf("s: %s\n", str);
+// 	ft_printf("The address is: %p, the value is %d\n", ptr, *ptr);
+// 	printf("The address is: %p, the value is %d\n", ptr, *ptr);
+// 	ft_printf("d: %d\n", d);
+// 	printf("d: %d\n", d);
+// 	ft_printf("x: %x\n", x);
+// 	printf("x: %x\n", x);
+// 	ft_printf("X: %X\n", X);
+// 	printf("X: %X\n", X);
+// 	ft_printf("u: %u\n", u);
+// 	printf("u: %u\n", u);
+// 	ft_printf("%%");
+// 	printf("\n");
+// 	printf("%%\n");
+// 	ft_printf("%%%");
+// 	printf("\n");
+// 	printf("%%%");
+// 	printf("\n");
+// }
+
+// return values
+
+// int	main()
+// {
+	// char		c = 'H';
+	// char          *str = "a stringy string";
+	// int			d = -903458;
+	// int	x = 986;
+	// int	*ptr = &x;
+
+	// ft_printf("Return value: %d", ft_printf("%c", c));
+	// printf("\n");
+	// printf("Return value: %d", printf("%c", c));
+	// printf("\n");
+	// ft_printf("Return value: %d", ft_printf("%s", str));
+	// printf("\n");
+	// printf("Return value: %d", printf("%s", str));
+	// printf("\n");
+	// ft_printf("ReturnValue: %d", ft_printf("%p", ptr));
+	// printf("\n");
+	// printf("ReturnValue: %d", printf("%p", ptr));
+	// printf("\n");
+	// ft_printf("Return value: %d", ft_printf("%d", d));
+	// printf("\n");
+	// printf("Return value: %d", printf("%d", d));
+	// printf("\n");
+// }
+
+// pointer test
 
 // int main()
 // {
